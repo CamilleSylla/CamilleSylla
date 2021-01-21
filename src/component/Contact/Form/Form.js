f5be39678ef6700369851031ab834757d13abfb2
Camille Sylla <camillesylla.web@gmail.com>
2021-01-21 12:26:49 +0100

    modify form mail sender

--- src/component/Contact/Form/Form.js
+++ src/component/Contact/Form/Form.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import './Form.css';
-import emailjs from 'emailjs-com'
 
 export default function Form () {
-    function sendEmail(e) {
-        e.preventDefault();
-    
-        emailjs.sendForm('gmail', 'template_a0yhol6', e.target, 'user_9Mwl1WzLHnTGVe4ZXHEMI')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-          e.target.reset();
-      }
     return (
             <div className="formInputs">
-                <form className="input-fields" onSubmit={sendEmail} >
+                <form  action="https://formspree.io/f/mdopyrdp" method="POST" className="input-fields" >
             <input type="text" placeholder="Nom et Prénom" name="name"/>
             <input type="text" placeholder="Sujet" name="Subject"/>
             <input type="email" placeholder="email" name="mail"/>
